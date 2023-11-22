3befc501924a7a48508722bae0d9c411aab7efea
RiccardoVoltolina <riky.volto@live.com>
2023-11-22 19:54:04 +0100

    create array in migration and implement index function for cocktail

--- app/Http/Controllers/API/CocktailController.php
+++ app/Http/Controllers/API/CocktailController.php
@@ -4,8 +4,29 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cocktail;
 
 class CocktailController extends Controller
 {
-    //
+
+    public function uploadFile(Request $request)
+{
+    $file = $request->file('file');
+    $content = file_get_contents($file->getRealPath());
+
+    $newFile = new Cocktail();
+    $newFile->name = $file->getClientOriginalName();
+    $newFile->content = $content;
+    $newFile->save();
+
+    return redirect()->back()->with('success', 'File uploaded successfully.');
+}
+
+
+    public function index (){
+        return response()->json([
+            'status' => 'Success',
+            'result' => Cocktail::all()->paginate(6),
+        ]);
+    }
 }
